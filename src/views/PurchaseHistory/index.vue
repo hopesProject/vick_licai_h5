8e4e6ce24c0cb8fd6e8387c979f80e42b9953ee3
huzheng12 <52150713+huzheng12@users.noreply.github.com>
2024-05-28 20:21:03 +0800

    隐藏邀请好友

--- src/views/PurchaseHistory/index.vue
+++ src/views/PurchaseHistory/index.vue
@@ -60,32 +60,38 @@
           <van-cell v-for="item in data" :key="item.id">
             <div class="item-box">
               <van-row>
-                <van-col span="12" style="text-align: left">span: 8</van-col>
-                <van-col span="12" style="text-align: right">span: 8</van-col>
+                <van-col span="12" style="text-align: left">
+                  <p class="title">理财产品1</p>
+                  <p>Investment</p>
+                  <p class="amount-p">₹7834.23</p>
+                  <p>Growth period</p>
+                  <p class="amount-p">₹7834.23</p>
+                </van-col>
+                <van-col span="12" style="text-align: right">
+                  <p class="status">进行中</p>
+                  <p>Daily returns</p>
+                  <p class="amount-p">₹7834.23</p>
+                  <p>Total returns</p>
+                  <p class="amount-p">₹7834.23</p>
+                </van-col>
+              </van-row>
+              <div class="ddjd">
+                {{ $t("订单进度") }}
+              </div>
+              <Progress :current="5" :total="30"></Progress>
+              <van-row>
+                <van-col span="12" style="text-align: left">
+                  <p class="butoon-box">{{ $t("购买时间") }}</p>
+                  <p class="butoon-box">{{ $t("到期时间") }}</p>
+                  <p class="butoon-box">{{ $t("当前累计收入") }}</p>
+                </van-col>
+                <van-col span="12" style="text-align: right">
+                  <p class="butoon-box">2024/02/02</p>
+                  <p class="butoon-box">2024/02/02</p>
+                  <p class="amount-p amount-butoon">₹7834.23</p>
+                </van-col>
               </van-row>
             </div>
-            <!-- <div class="item-box">
-              <img :src="item.img" alt="" />
-              <div class="content">
-                <div class="mb">
-                  {{ $t("商品名称：") }}{{ item.productTitle }}
-                </div>
-                <div>{{ $t("购买金额：") }}{{ item.price * item.num }}</div>
-              </div>
-              <div class="time">
-                <div>
-                  {{ $t("购买时间：")
-                  }}<span>
-                    {{ item.createTime | _timeFormat("YYYY-MM-DD") }}
-                  </span>
-                </div>
-                <div>
-                  {{ $t("到期时间：")
-                  }}<span> {{ item.endTime | _timeFormat("YYYY-MM-DD") }}</span>
-                </div>
-                <div></div>
-              </div>
-            </div> -->
           </van-cell>
         </van-list>
       </van-pull-refresh>
@@ -117,7 +123,7 @@ export default {
     };
   },
   created() {
-    // this.getList();
+    this.getList();
   },
   methods: {
     async getList(isRefreshing) {
@@ -142,7 +148,9 @@ export default {
         // if (this.data.length >= res.data.total) {
         this.finished = true;
         // }
-      } catch (error) {}
+      } catch (error) {
+        console.log(error);
+      }
       this.finished = true;
     },
     onClickLeft() {
@@ -154,17 +162,65 @@ export default {
 
 <style lang="scss" scoped>
 .page {
-  height: 100vh;
+  height: 100%;
 }
 .item-box {
   padding: 18px 25px;
   margin: 0 auto 10px;
   width: 690px;
-  height: 548px;
   border-radius: 32px;
   opacity: 1;
   background: #ffffff;
   box-shadow: 0px 12.55px 56.45px 0px rgba(0, 0, 0, 0.1);
+  font-variation-settings: "opsz" auto;
+  font-size: 24px;
+  font-weight: normal;
+  line-height: 30px;
+  letter-spacing: 0.6px;
+  color: #818197;
+  .butoon-box {
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 32px;
+
+    letter-spacing: 0.6px;
+    color: #666666;
+    margin-top: 23px;
+  }
+  .amount-butoon {
+    margin-top: 23px;
+  }
+  .title {
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 32px;
+    color: #3d3d3d;
+    margin-bottom: 17px;
+  }
+  .status {
+    font-size: 24px;
+    font-weight: 500;
+    color: #ff752f;
+    margin-bottom: 17px;
+  }
+  .amount-p {
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 32px;
+    letter-spacing: 0.6px;
+    color: #d35a18;
+    margin-bottom: 12px;
+  }
+  .ddjd {
+    padding-top: 24px;
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 32px;
+    letter-spacing: 0.6px;
+
+    color: #3d3d3d;
+    margin-bottom: 24px;
+  }
 }
 header {
   height: 255px;
@@ -215,6 +271,7 @@ header {
     letter-spacing: -0.64px;
     color: #ffffff;
     z-index: 0;
+    margin-bottom: 10px;
   }
 }
 
