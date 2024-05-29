ec232fc97bdb5302ffb7e4036e39a3decab6bfb4
gongmq <1139701003@qq.com>
2024-05-29 23:14:44 +0800

    feat: 充值  VIP 客服 佣金比例

--- src/views/vip-list/index.vue
+++ src/views/vip-list/index.vue
@@ -1,239 +1,171 @@
 <template>
-  <div class="vip-warp">
-    <come-back />
-    <header>{{ $t("VIP等级") }}</header>
-    <div class="ifyao"># {{ $t("VIP升级条件") }} #</div>
-    <div class="content-warp">
-      <van-row>
-        <van-col span="8">- {{ $t("VIP等级") }} </van-col>
-        <van-col span="8">{{ $t("成长值") }}</van-col>
-        <van-col span="8">{{ $t("距离升级") }} -</van-col>
-      </van-row>
-      <van-row
-        class="data-item"
-        type="flex"
-        justify="center"
-        align="center"
-        v-for="item in data"
-        :key="item.id"
-      >
-        <van-col span="8" class="relative">
-          <img :src="item.img" alt="" />
-          <div class="relatives">{{ item.level }}</div>
-        </van-col>
-        <van-col span="8">{{ item.amount }}</van-col>
-        <van-col span="8" class="right-text">
-          <van-progress
-            :percentage="
-              (userInfo.sumBuyAmount / item.amount) * 100 >= 100
-                ? 100
-                : (userInfo.sumBuyAmount / item.amount) * 100
-            "
-          />
-          <div class="trest">
-            {{
-              (userInfo.sumBuyAmount / item.amount) * 100 >= 100
-                ? $t("已达成")
-                : $t("未达成")
-            }}
-          </div>
-        </van-col>
-      </van-row>
+    <div class="wrapper">
+        <van-nav-bar title="VIP" :border="false" left-arrow @click-left="$router.go(-1);"></van-nav-bar>
+        <div class="content">
+            <div class="content-img"><img src="@/assets/img/vip-grade.png" alt=""></div>
+            <div class="content-box">
+                <div class="box-title">VIP升级条件</div>
+                <div class="box-list" v-for="(item, i) in list" :key="i">
+                    <img src="@/assets/img/commission-icon3.png" alt="">
+                    <div class="list-name">{{ item.name }}</div>
+                    <div class="list-txt"><span>成长值</span><span>{{ item.num }}</span></div>
+                    <div class="list-progress">
+                        <span>未达成</span>
+                        <van-progress :percentage="item.percentage" stroke-width="18" track-color="#F0F0F0"
+                            color="linear-gradient(90deg, #FF8700 0%, #FF6200 117%)" />
+                    </div>
+                </div>
+            </div>
+            <div class="illustrate">
+                <div class="illustrate-title">充值说明</div>
+                <pre class="illustrate-txt">
+                1·当你正确完成付款，资金会在20分您得到的账户当中
+                2·当你正确完成付款，资金会在20分钟到达您得到的账户
+                3·当你正确完成付款，资金会在20分钟到达您得到的账</pre>
+            </div>
+        </div>
     </div>
-
-    <div class="illustrate">
-      <div class="header-box">{{ $t("等级说明") }}</div>
-      <div class="text">
-        <p>
-          Upgrade to VIP and enjoy unparalleled benefits! The higher your VIP
-          level, the more rewards and privileges you will receive. Priority
-          service, exclusive offers, and generous rewards make your experience
-          even more outstanding. Take action now and embark on a VIP exclusive
-          journey!
-        </p>
-      </div>
-    </div>
-  </div>
 </template>
-
 <script>
-import { getLevelSetting } from "@/api";
-import { mapGetters } from "vuex";
-
 export default {
-  computed: {
-    ...mapGetters(["userInfo"]),
-  },
-  data() {
-    return {
-      touxiang: require("@/assets/touxiangvip.png"),
-      data: [],
-    };
-  },
-  mounted() {
-    this.getList();
-  },
-  methods: {
-    async getList() {
-      const res = await getLevelSetting();
-      if (res.status === 0) {
-        this.data = res.data;
-      }
-    },
-  },
-};
-</script>
-
-<style lang="scss" scoped>
-.vip-warp {
-  width: 100vw;
-  min-height: 100vh;
-  background-image: url("@/assets/vip-bg.png");
-  background-size: 100%;
-  background-repeat: no-repeat;
-
-  .illustrate {
-    width: 686px;
-    min-height: 100px;
-    background: rgba(253, 208, 197, 0.6);
-    border-radius: 40px;
-    margin: 0 auto;
-    margin-top: 24px;
-    font-size: 34px;
-    font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
-
-    font-weight: normal;
-    text-align: left;
-    color: #ffffff;
-    line-height: 34px;
-    padding: 50px;
-
-    .header-box {
-      font-size: 34px;
-      font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
-
-      font-weight: normal;
-      color: #ffffff;
-      line-height: 34px;
-      margin-bottom: 22px;
-      text-align: center;
-    }
-
-    .text {
-      font-size: 26px;
-      font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
-
-      font-weight: bold;
-      text-align: left;
-      color: #ffffff;
-      letter-spacing: 4.68px;
-    }
-  }
-
-  .content-warp {
-    width: 686px;
-    background: rgba(253, 208, 197, 0.6);
-    border-radius: 40px;
-    margin: 0 auto;
-    margin-top: 24px;
-    font-size: 34px;
-    font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
-
-    font-weight: normal;
-    text-align: left;
-    color: #ffffff;
-    line-height: 34px;
-
-    padding: 47px 33px 91px;
-
-    .data-item {
-      margin-top: 20px;
-      background: #ffffff;
-      border-radius: 40px;
-      box-shadow: -10px 0px 17px 1.5px rgba(20, 7, 0, 0.1);
-      font-size: 33px;
-      font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
-
-      font-weight: 400;
-      text-align: left;
-      color: #222222;
-      line-height: 33px;
-      padding: 40px 60px;
-      margin-bottom: 6px;
-
-      .right-text {
-        font-size: 26px;
-        font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
-
-        font-weight: 400;
-        text-align: left;
-        color: #ff977c;
-        line-height: 33px;
-
-        .trest {
-          text-align: center;
-          margin-top: 14px;
+    data() {
+        return {
+            list: [
+                {
+                    name: 'TOP1',
+                    num: 12,
+                    percentage: 70
+                },
+                {
+                    name: 'TOP2',
+                    num: 12,
+                    percentage: 80
+                },
+                {
+                    name: 'TOP3',
+                    num: 12,
+                    percentage: 80
+                },
+            ]
         }
-      }
-
-      .relatives {
-        position: absolute;
-        top: 40px;
-        left: 45px;
-        font-size: 19px;
-        font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
-
-        font-weight: bold;
-        text-align: left;
-        color: #ffffff;
-        line-height: 19px;
-      }
-
-      img {
-        width: 67px;
-        height: 67px;
-      }
     }
-  }
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+    min-height: 100vh;
+    background-color: #f8f3f2;
+    background-image: url('@/assets/img/vip-bg.png');
+    background-size: 100%;
+    overflow: auto;
 
-  header {
-    font-size: 145px;
-    font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
+    .van-nav-bar {
+        background-color: transparent;
 
-    font-weight: bold;
-    text-align: center;
-    color: #fefefe;
-    letter-spacing: 1.45px;
-    text-shadow: 7.07px 7.07px 5px 10px rgba(25, 92, 190, 0.54);
-    margin-bottom: 26px;
-  }
-
-  .ifyao {
-    margin: 0 auto;
-    width: 496px;
-    line-height: 53px;
-    background: #ffffff;
-    border-radius: 27px;
-    font-size: 30px;
-    font-family: "Varela Round", sans-serif, sans-serif, Georgia, serif !important;
-
-    font-weight: normal;
-    text-align: center;
-    color: #fc8c70;
-    letter-spacing: 2.25px;
-  }
+        :deep(.van-nav-bar__title) {
+            color: #fff;
+        }
+    }
 }
 
-::v-deep .van-progress {
-  height: 25px;
-  border-radius: 17px;
+.content {
+    margin: 16px;
 }
 
-::v-deep .van-progress__portion {
-  background: #ff957c;
+.content-img {
+    margin-top: 80px;
+    width: 360px;
+    height: 230px;
+
+    >img {
+        width: 100%;
+        height: 100%;
+    }
 }
 
-::v-deep .van-progress__pivot {
-  display: none;
+.content-box {
+    border-radius: 16px;
+    background: #FFFFFF;
+    padding: 36px 34px;
+    box-shadow: 0px 4px 18px 0px rgba(0, 0, 0, 0.1);
+
+    .box-title {
+        font-size: 32px;
+        color: #3D3D3D;
+        text-align: center;
+    }
+
+    .box-list {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-size: 24px;
+        color: #3D3D3D;
+        border-radius: 40px;
+        border: 2px solid #FF8700;
+        padding: 48px 36px;
+        margin-top: 32px;
+
+        >img {
+            width: 72px;
+            height: 72px;
+            border-radius: 24px;
+        }
+
+        .list-name {
+            font-size: 32px;
+            padding-right: 16px;
+            font-weight: 600;
+        }
+
+        .list-txt {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-between;
+
+            >span:nth-child(1) {
+                padding-bottom: 12px;
+            }
+        }
+
+        .list-progress {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            .van-progress {
+                margin-top: 16px;
+                width: 220px;
+                border-radius: 40px;
+
+                :deep(.van-progress__pivot) {
+                    font-size: 18px;
+                    background: transparent !important;
+                    color: #000;
+                }
+            }
+        }
+    }
+}
+
+.illustrate {
+    margin: 36px 0;
+    font-weight: 600;
+
+    .illustrate-title {
+        font-size: 36px;
+        color: #FFFFFF;
+        text-align: center;
+        margin-bottom: 18px;
+    }
+
+    .illustrate-txt {
+        white-space: pre-line;
+        font-size: 24px;
+        font-weight: 600;
+        line-height: 42px;
+        color: #F6F6F6;
+    }
 }
 </style>
